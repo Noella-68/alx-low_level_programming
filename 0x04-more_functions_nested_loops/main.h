dea39fb47eeb684aac57c58dd58bd05bc2229361
Noella Jarega <noellajarega@gmail.com>
2022-09-15 15:34:54 -0700

    Last

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stdio.h>
+
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+
+#endif /* HOLBERTON_H */
